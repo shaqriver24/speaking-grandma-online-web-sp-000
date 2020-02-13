5a79d06608e6415d629e86b1de36b4e30ae0aa2c
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-13 06:34:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
-  if sting == "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     "NO, NOT SINCE 1938!"
